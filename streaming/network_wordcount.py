7c0a5d008bbf5c0265ec3b40b42ebf0176a9146f
Boni García <boni.garcia@uc3m.es>
2020-03-25 10:35:27 +0100

    Enchance comments in streaming network word count example

--- streaming/network_wordcount.py
+++ streaming/network_wordcount.py
@@ -1,21 +1,21 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
-# Create a local StreamingContext with * working thread and batch interval of 5 seconds
+# Local SparkContext using * threads, depending on the number of logical processors
 sc = SparkContext("local[*]", "NetworkWordCount")
+
+# Create a local StreamingContext with a batch interval of 5 seconds
 ssc = StreamingContext(sc, 5)
 
 # Create a DStream that will connect to hostname:port
 lines = ssc.socketTextStream("localhost", 9999)
 
-# Split each line into words
+# Split each line into words and count words in each batch
 words = lines.flatMap(lambda line: line.split(" "))
-
-# Count each word in each batch
 pairs = words.map(lambda word: (word, 1))
 wordCounts = pairs.reduceByKey(lambda x, y: x + y)
 
-# Print the first ten elements of each RDD generated in this DStream to the console
+# Print the word count of each RDD generated in this DStream to the standard output
 wordCounts.pprint()
 
 ssc.start()             # Start the computation
