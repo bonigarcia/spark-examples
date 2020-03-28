9c72d85eba70316253583e73219b6901710e9deb
Boni García <boni.garcia@uc3m.es>
2020-03-28 11:42:17 +0100

    Simplify Flume example

--- streaming/flume_wordcount.py
+++ streaming/flume_wordcount.py
@@ -1,31 +1,22 @@
-import sys
-
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.flume import FlumeUtils
+
 
 def quiet_logs(sc):
     logger = sc._jvm.org.apache.log4j
     logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print(
-            "Usage: " + sys.argv[0] + " <hostname> <port>",
-            file=sys.stderr)
-        sys.exit(-1)
 
-    conf = (SparkConf()
-            .setAppName("FlumeLogger")
-            .set("spark.jars.packages", "org.apache.spark:spark-streaming-flume_2.11:2.4.5"))
-    sc = SparkContext(conf=conf)
-    quiet_logs(sc)
-    ssc = StreamingContext(sc, 1)
+sc = SparkContext(conf=SparkConf()
+                  .setAppName("FlumeLogger")
+                  .set("spark.jars.packages", "org.apache.spark:spark-streaming-flume_2.11:2.4.5"))
+quiet_logs(sc)
+ssc = StreamingContext(sc, 1)
 
-    hostname, port = sys.argv[1:]
-    kvs = FlumeUtils.createStream(ssc, hostname, int(port))
-    lines = kvs.map(lambda x: x[1])
-    lines.pprint()
+kvs = FlumeUtils.createStream(ssc, "localhost", 4444)
+lines = kvs.map(lambda x: x[1])
+lines.pprint()
 
-    ssc.start()
-    ssc.awaitTermination()
+ssc.start()
+ssc.awaitTermination()
