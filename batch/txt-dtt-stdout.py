87432538c7693e2876023dffdbc26c997a41c251
Boni García <boni.garcia@uc3m.es>
2020-03-30 16:35:05 +0200

    Separate RDDs in different variables

--- batch/txt-dtt-stdout.py
+++ batch/txt-dtt-stdout.py
@@ -1,16 +1,16 @@
 from pyspark import SparkContext
 
 # Local SparkContext
-sc = SparkContext(master="local[*]", appName="file-DTT-stdout")
+sc = SparkContext(master="local[*]", appName="textFile-DTT-StdOut")
 
 # 1. Input data: text file
-lines = sc.textFile("../data/hamlet.txt")
+linesRdd = sc.textFile("../data/hamlet.txt")
 
 # 2. Data processing: word count
-words = lines.flatMap(lambda line: line.split(" "))
-pairs = words.map(lambda s: (s, 1))
-counts = pairs.reduceByKey(lambda a, b: a + b)
-wordcount = counts.collect()
+wordsRdd = linesRdd.flatMap(lambda line: line.split(" "))
+pairsRdd = wordsRdd.map(lambda s: (s, 1))
+countsRdd = pairsRdd.reduceByKey(lambda a, b: a + b)
+wordcount = countsRdd.collect()
 
 # 3. Output data: show result in the console
 print(wordcount)
