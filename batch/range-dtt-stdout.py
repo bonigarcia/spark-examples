77e1e6fc451f01e04527d0c18a4b9a51f9812781
Boni García <boni.garcia@uc3m.es>
2020-03-30 12:25:23 +0200

    Enhance comments in first batch example

--- batch/range-dtt-stdout.py
+++ batch/range-dtt-stdout.py
@@ -1,24 +1,25 @@
-from pyspark import SparkConf, SparkContext
+from pyspark import SparkContext
 
-# Create SparkContext in local using * threads, depending on the number of logical processors
-sc = SparkContext("local[*]", "HelloWorld")
+# Local SparkContext using N threads (N = number of logical processors)
+sc = SparkContext(master="local[*]", appName="range-DTT-stdout")
 
-# Input data: integer list (unstructured batch)
+# 1. Input data: list of integers (unstructured batch)
 data = range(1, 10001)
-print("Input data has " + str(len(data)) + " integers from " +
+print("Input data: " + str(len(data)) + " integers from " +
       str(data[0]) + " to " + str(data[9999]))
 
-# Parallelize input data using * partitions
-# This operation is a transformation of data into an RDD.
-# Spark uses lazy evaluation, so no Spark jobs are run at this point
+# 2. Data processing
+# Parallelize input data into a RDD (lazy evaluation) using * partitions
 rangeRDD = sc.parallelize(data)
 print("RDD has been created using " +
       str(rangeRDD.getNumPartitions()) + " partitions")
 
-# Process data using different lambda functions:
-# substract 1 to all elements and then select those lower than 10
+# Process data using lambda functions and collect results:
+# 1) substract 1 to all elements. 2) select those lower than 10.
 out = (rangeRDD
        .map(lambda y: y - 1)
        .filter(lambda x: x < 10)
        .collect())
+
+# 3. Output data: show result in the standard output (console)
 print("The output is " + str(out))
