06320b2b66c353451a264cbf51e5a294d235d0c5
Boni García <boni.garcia@uc3m.es>
2020-03-31 17:53:31 +0200

    Unify style of SparkSession creation in Cassandra example

--- batch/json-dataframe-cassandra.py
+++ batch/json-dataframe-cassandra.py
@@ -2,13 +2,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, StringType
 from uuid import uuid4
 
-spark = SparkSession \
-    .builder \
-    .appName("JsonDataFrameCassandra") \
-    .config("spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.11:2.4.3") \
-    .config("spark.cassandra.connection.host", "localhost") \
-    .config("spark.cassandra.connection.port", "9042") \
-    .getOrCreate()
+spark = (SparkSession
+         .builder
+         .master("local[*]")
+         .appName("JsonDataFrameCassandra")
+         .config("spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.11:2.4.3")
+         .config("spark.cassandra.connection.host", "localhost")
+         .config("spark.cassandra.connection.port", "9042")
+         .getOrCreate())
 spark.sparkContext.setLogLevel("ERROR")
 
 # Create DataFrame object from JSON file
