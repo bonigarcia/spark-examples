ef0a01e76602da661c20d6a39224f64896d783a5
Boni García <boni.garcia@uc3m.es>
2020-04-05 16:10:50 +0200

    Enhance comments in Kinesis example

--- streaming/kinesis-dstream-stdout.py
+++ streaming/kinesis-dstream-stdout.py
@@ -10,17 +10,24 @@ if __name__ == "__main__":
               file=sys.stderr)
         sys.exit(-1)
 
+    # Local SparkContext and StreamingContext (batch interval of 1 second)
     sc = SparkContext(master="local[*]",
                       appName="Kinesis-DStream-StdOut",
                       conf=SparkConf()
                       .set("spark.jars.packages", "org.apache.spark:spark-streaming-kinesis-asl_2.11:2.4.5"))
     ssc = StreamingContext(sc, 1)
+
+    # 1. Input data: create a DStream from Kinesis
     appName, streamName, endpointUrl, regionName = sys.argv[1:]
     lines = KinesisUtils.createStream(
         ssc, appName, streamName, endpointUrl, regionName, InitialPositionInStream.LATEST, 2)
+
+    # 2. Data processing: word count
     counts = lines.flatMap(lambda line: line.split(" ")) \
         .map(lambda word: (word, 1)) \
         .reduceByKey(lambda a, b: a+b)
+
+    # 3. Output data: show result in the console
     counts.pprint()
 
     ssc.start()
