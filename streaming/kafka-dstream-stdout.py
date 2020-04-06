eb01b794eb1d6ba65ff2270b73f5404828c1fd57
Boni García <boni.garcia@uc3m.es>
2020-04-06 13:08:42 +0200

    Improve comments in Kafka DStream example

--- streaming/kafka-dstream-stdout.py
+++ streaming/kafka-dstream-stdout.py
@@ -2,21 +2,25 @@ from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
-
+# Local SparkContext and StreamingContext (batch interval of 5 seconds)
 sc = SparkContext(master="local[*]",
                   appName="Kafka-DStream-StdOut",
                   conf=SparkConf()
                   .set("spark.jars.packages", "org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.5"))
 ssc = StreamingContext(sc, 5)
 
-kvs = KafkaUtils.createStream(
-    ssc, "localhost:2181", "spark-streaming-consumer", {"test-topic": 1})
+# 1. Input data: create a DStream from Apache Kafka
+topic = {"test-topic": 1}  # Topic dictionary (topic-name : num-partitions)
+stream = KafkaUtils.createStream(
+    ssc, "localhost:2181", "spark-streaming-consumer", topic)
 
-lines = kvs.map(lambda x: x[1])
-counts = lines.flatMap(lambda line: line.split(" ")) \
-    .map(lambda word: (word, 1)) \
-    .reduceByKey(lambda a, b: a+b)
+# 2. Data processing: word count
+counts = (stream.map(lambda x: x[1])
+          .flatMap(lambda line: line.split(" "))
+          .map(lambda word: (word, 1))
+          .reduceByKey(lambda a, b: a+b))
 
+# 3. Output data: show result in the console
 counts.pprint()
 
 ssc.start()
