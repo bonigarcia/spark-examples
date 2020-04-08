88aae87d11e72a1d5a49a5cfff69759c775f982e
Boni García <boni.garcia@uc3m.es>
2020-04-08 12:47:49 +0200

    Simplify quiet log in Flume example

--- streaming/flume-dstream-stdout.py
+++ streaming/flume-dstream-stdout.py
@@ -2,17 +2,12 @@ from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.flume import FlumeUtils
 
-def quiet_logs(sc):
-    logger = sc._jvm.org.apache.log4j
-    logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
-
-
 # Local SparkContext and StreamingContext (batch interval of 1 second)
 sc = SparkContext(master="local[*]",
                   appName="Flume-DStream-StdOut",
                   conf=SparkConf()
                   .set("spark.jars.packages", "org.apache.spark:spark-streaming-flume_2.11:2.4.5"))
-quiet_logs(sc)
+sc.setLogLevel("ERROR")
 ssc = StreamingContext(sc, 1)
 
 # 1. Input data: create a DStream from Apache Flume
