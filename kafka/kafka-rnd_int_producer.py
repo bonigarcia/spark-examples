d6747b8107f915a5a71103e23851db92cad6c5c8
Boni García <boni.garcia@uc3m.es>
2020-04-11 14:18:07 +0200

    Include Kafka producer for random integers

---
+++ kafka/kafka-rnd_int_producer.py
@@ -0,0 +1,17 @@
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+from random import randrange
+import time
+
+# Reference: https://kafka-python.readthedocs.io/en/master/index.html
+producer = KafkaProducer(bootstrap_servers=["localhost:9092"])
+startTime = time.time()
+waitSeconds = 1.0
+
+while True:
+    randomInt = randrange(100)
+    print("Sending random number to Kafka", randomInt)
+    producer.send("test-topic", str(randomInt).encode())
+
+    # Wait a number of second until next message
+    time.sleep(waitSeconds - ((time.time() - startTime) % waitSeconds))
